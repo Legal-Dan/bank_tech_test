98021794786109bdc61342b68313a2182656c04d
Daniel Clements <clemies2000@hotmail.com>
2021-10-25 12:15:17 +0100

    Deposit and withdrawal data is stored against the account

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,15 +1,31 @@
+require 'date'
+
 class Bank_account
   attr :balance
+  attr :statement
   
   def initialize
     @balance = 0
+    @statement = []
   end
 
   def deposit(credit)
     @balance += credit
+    update_statement(@balance, credit=credit)
   end
 
   def withdraw(debit)
     @balance -= debit
+    update_statement(@balance, debit=debit)
   end
+
+  def update_statement(balance, credit=nil, debit=nil)
+    @statement << {
+      date: Date.today.strftime("%d/%m/%Y"),
+      credit: credit,
+      debit: debit,
+      balance: balance
+    }
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,4 +1,5 @@
 require 'bank'
+require 'date'
 
 describe 'bank_account' do
   it 'can store a sum that is deposited' do
@@ -20,4 +21,12 @@ describe 'bank_account' do
     account.withdraw(50)
     expect(account.balance).to equal(50)
   end
+
+  it 'can store the details of the transaction including the date' do
+    account = Bank_account.new
+    allow(Date).to receive(:today).and_return Date.new(2023,1,10)
+    account.deposit(100)
+    expect(account.statement[0][:date]).to eq('10/01/2023')
+  end
+
 end
